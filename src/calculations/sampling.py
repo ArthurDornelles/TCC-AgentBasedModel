db8b0a4606123c14273172ee34c929c47b70007e
ArthurDornelles <arthurdornelles19@gmail.com>
2023-03-21 17:39:33 -0300

    feat: remodels transaction sampling

--- src/calculations/sampling.py
+++ src/calculations/sampling.py
@@ -8,37 +8,45 @@ def sample(array: np.array) -> np.array:
     - each row is a state and each pair of columns is a combination
     array[1,0:2] is a combination
     """
-    sampling_array = check_even_size_state_array(
-        {
-            state: np.random.choice(
-                array[array[:, 1] == state][:, 0],
-                size=array[array[:, 1] == state][:, 0].size,
+    sampling_array = np.array(
+        [
+            np.random.choice(
+                array[:, 0],
+                size=int(array[:, 0].size / 2),
                 replace=True,
-                p=probability_array(array, state),
+                p=probability_array(array),
             )
-            for state in np.sort(np.unique(array[:, 1]))
-        }
+        ]
+    )
+    sampling_array = np.array(
+        [
+            np.array(
+                [
+                    agent,
+                    np.random.choice(
+                        array[
+                            (array[:, 1] == array[array[:, 0] == agent][:, 1])
+                            & ~(array[:, 0] == agent)
+                        ][:, 0],
+                        size=1,
+                        replace=True,
+                        p=probability_array(
+                            array[
+                                (array[:, 1] == array[array[:, 0] == agent][:, 1])
+                                & ~(array[:, 0] == agent)
+                            ]
+                        ),
+                    )[0],
+                ]
+            )
+            for agent in sampling_array[0]
+        ]
     )
 
-    sampling_transaction_array = {
-        state_key: np.array(
-            [transaction for transaction in zip(state_array[::2], state_array[1::2])]
-        )
-        for state_key, state_array in sampling_array.items()
-    }
-
-    return sampling_transaction_array
+    return sampling_array
 
 
-def check_even_size_state_array(array: dict[np.array]) -> dict:
-    array = {
-        state: array[state] if len(array[state]) % 2 == 0 else array[state][:-1]
-        for state in array.keys()
-    }
-    return array
-
-
-def probability_array(array: np.array, state: int) -> np.array:
-    sqrt_array = array[array[:, 1] == state][:, 2] ** exchange_fuzzy_probability
+def probability_array(array: np.array) -> np.array:
+    sqrt_array = array[:, 2] ** exchange_fuzzy_probability
     probability_array = sqrt_array / sqrt_array.sum()
     return probability_array
